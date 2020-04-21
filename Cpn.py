ecdfd39fcce08df8d565cd7b173c13a93644ccac
Boo-boop <64065173+Boo-boop@users.noreply.github.com>
2020-04-21 13:06:37 +0500

    Add files via upload

---
+++ Cpn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This notebook is mainly for the Coursera Capstone project
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
